d0f847a31b687681030e822d5399fab4e465fa56
RuanVNBezerra <164013416+RuanVNBezerra@users.noreply.github.com>
2024-10-19 16:54:43 -0300

    Adicionar arquivos de projeto.

---
+++ ApredendoHashSet/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ApredendoHashSet;
+
+internal class Program
+{
+    static void Main()
+    {
+        //HashSet é uma lista de valores unicos
+        HashSet<int> qualquerCoisa = new HashSet<int>();
+        // adcionando itens
+        qualquerCoisa.Add(1);
+        qualquerCoisa.Add(2);
+        qualquerCoisa.Add(1); // valor repetido
+
+        Console.WriteLine(qualquerCoisa.Count);
+        // o valor repetido não será contado como um item adcionado, pois esse valor já existe!
+    }
+}
